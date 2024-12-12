03f8cfe22b20b8ad49089f58bb919da196020f95
baghelharsh200 <159938238+baghelharsh200@users.noreply.github.com>
2024-12-12 20:43:12 +0530

    Create UserController.java

---
+++ src/main/java/com/madridpay/UserController.java
@@ -0,0 +1,15 @@
+package com.madridpay.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Madrid Payment App!";
+    }
+}
